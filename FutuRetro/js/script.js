de52846f3afd308f3e6ef89e5c46d1e89f5346f9
Penelope-M <90867349+Penelope-M@users.noreply.github.com>
2023-03-17 15:24:01 -0400

    dfgd

--- FutuRetro/js/script.js
+++ FutuRetro/js/script.js
@@ -2,10 +2,27 @@ const swiper = new Swiper('.mySwiper', {
     // Optional parameters
     direction: 'horizontal',
     loop: true,
-  
+    slidesPerView: 1,
+    centeredSlides: true,
+    cssMode: true,
     // Navigation arrows
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    // when window width is >= 1200px
+    1200: {
+      slidesPerView: 3,
+      spaceBetween:25
+      
+    },
+  },
+  coverflowEffect: {
+    rotate: 50,
+    stretch: 0,
+    depth: 100,
+    modifier: 1,
+    slideShadows: true,
   },
 });
